ffe58861cd7ae257c5755129146f42f414082636
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-10-19 17:14:05 +0300

    login&getUsers

--- src/controllers/authController/authController.js
+++ src/controllers/authController/authController.js
@@ -1,7 +1,15 @@
 const User = require('../../models/user');
 const Role = require('../../models/role');
 const bcrypt = require('bcryptjs');
-const { validationResult } = require('express-validator')
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const {secret} = require('../../../config');
+
+
+const generateAccessToken = (id, roles) => {
+  const payload = {id, roles};
+  return jwt.sign(payload, secret, {expiresIn: '24h'})
+}
 
 
 class AuthController {
@@ -14,29 +22,39 @@ class AuthController {
             const candidate = await User.findOne({username});
             if (candidate) return res.status(400).json({message: 'Пользователь с таким именем уже существует'});
             const hashPassword = bcrypt.hashSync(password, 7);
-            const userRole = await Role.findOne({value: 'USER'});
+            const userRole = await Role.findOne({value: 'ADMIN'});
             const user = new User({username, password: hashPassword, roles: [userRole.value]});
             await user.save();
 
-            return res.json({message: 'User created successful'})
+            return res.json({message: 'User created successful'});
         } catch (e) {
-            console.log(e)
-            res.status(400).json({message:'Registration error'})
+            console.log(e);
+            res.status(400).json({message:'Registration error'});
         }
     }
 
     async login (req,res) {
         try {
+            const {username, password} = req.body;
 
+            const user = await User.findOne({username});
+            if (!user) return res.status(400).json({message: `User ${user} is not found`});
+
+            const validPassword = bcrypt.compareSync(password, user.password);
+            if (!validPassword) return res.status(400).json({message: 'Password is not correct'});
+
+            const token = generateAccessToken(user._id, user.roles);
+            return res.json({token});
         } catch (e) {
-            console.log(e)
-            res.statusCode(400).json('Login error')
+            console.log(e);
+            res.status(400).json('Login error');
         }
     }
 
     async getUsers (req,res) {
         try {
-            res.json('server work')
+            const users = await User.find()
+            res.json(users)
         } catch (e) {
 
         }
